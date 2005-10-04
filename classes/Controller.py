091055e8abcf1ae763c70dbc8dc405e5e9e4a350
freddie <freddie@35f662cf-58f2-0310-8820-d172d141b225>
2005-10-04 04:16:11 +0000

    Add code to generate the list of articles we need to post.

--- classes/Controller.py
+++ classes/Controller.py
@@ -33,6 +33,7 @@
 import asyncore
 import os
 import select
+from cStringIO import StringIO
 
 from classes import yEnc
 
@@ -41,16 +42,73 @@ from classes import yEnc
 class Controller:
 	def __init__(self, conf):
 		self.conf = conf
-		print self.conf
+		
+		self._articles = []
+		self._files = {}
 		
 		# Set up our poller
 		asyncore.poller = select.poll()
 	
 	def post(self, dirs):
+		self.generate_part_list(dirs)
+	
+	# -----------------------------------------------------------------------
+	# Generate the list of articles we need to post
+	def generate_article_list(self, dirs):
 		for dirname in dirs:
-			files = os.listdir(dirname)
-			postme = []
+			if dirname.endswith(os.sep):
+				dirname = dirname[:-len(os.sep)]
+			if not dirname:
+				continue
+			
+			article_size = self.conf['posting']['article_size']
+			
+			# Get a list of useful files
+			f = os.listdir(dirname)
+			files = []
+			for filename in f:
+				filepath = os.path.join(dirname, filename)
+				# Skip non-files and empty files
+				if os.path.isfile(filepath) and os.path.getsize(filepath):
+					files.append(filename)
+			files.sort()
+			
+			n = 1
 			for filename in files:
 				filepath = os.path.join(dirname, filename)
+				filesize = os.path.getsize(filepath)
+				
+				full, partial = divmod(filesize, article_size)
+				if partial:
+					parts = full + 1
+				else:
+					parts = full
+				
+				# Build a subject
+				temp = '%%0%sd' % (len(str(len(files))))
+				filenum = temp % (n)
+				subject = '%s [%s/%d] - "%s" yEnc (%%s/%d)' % (dirname, filenum, len(files), filename, parts)
+				
+				# Now make up our parts
+				for i in range(parts):
+					article = [filepath, subject, i+1]
+					self._articles.append(part)
+				
+				n += 1
+	
+	# -----------------------------------------------------------------------
+	# Build an article for posting.
+	def build_article(self):
+		(f, filepathfilesize) = self._files[0]
+		#if f is None:
+		#	self._files[0][0] = f = open(
+		
+		#self._part += 1
+		data = f.read(self.conf['posting']['article_size'])
+		
+		# If we've just hit the end of the file, we can throw it away now
+		if (self._part * self.conf['posting']['article_size'] > filesize):
+			f.close()
+			self._files.pop(0)
 
 # ---------------------------------------------------------------------------
