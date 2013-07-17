5e6bf8c5786774025a9aa8ad50672bf8183689a5
madcowfred <freddie@wafflemonster.org>
2013-07-17 20:51:08 +0930

    Add workaround for Python 2.6's lack of OrderedDict

--- newsmangler/article.py
+++ newsmangler/article.py
@@ -25,7 +25,10 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-from collections import OrderedDict
+try:
+    from collections import OrderedDict
+except ImportError:
+    from ordereddict import OrderedDict
 from cStringIO import StringIO
 
 from newsmangler.yenc import yEncode
